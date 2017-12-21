039045bb912189ae946bbf0b7476c11949f1ce70
Ben Rodenhäuser <ben@karo.berlin>
2017-12-21 11:44:17 +0100

    use round(2) in both retrieve and insert

--- lib/set_map.rb
+++ lib/set_map.rb
@@ -58,7 +58,7 @@ class SetMap
     raise(SetError, 'Invalid value') unless self.class.valid_score?(val)
     old_score = self[key]
     @hash[key] = [self[key] + val, self.class.max_score].min
-    @size = (@size + (self[key] - old_score)).round(1)
+    @size = (@size + (self[key] - old_score)).round(2)
     self
   end
 
